7879508da44a0859e3499ec00db696c3aee1a021
Himani <oyo@MacBook-Pro-64.local>
2020-09-15 17:12:00 +0530

    Updates

---
+++ precache-manifest.edbae7414b55e9321359120b2686d092.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e57758cf22d5c17a01b084ad6128dd12",
+    "url": "/instagram-clone/index.html"
+  },
+  {
+    "revision": "1cb745d9c3a7b9333a74",
+    "url": "/instagram-clone/static/css/main.a67b3c0a.chunk.css"
+  },
+  {
+    "revision": "5b27b54714fdd0a5abf9",
+    "url": "/instagram-clone/static/js/2.b4a95212.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/instagram-clone/static/js/2.b4a95212.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cb745d9c3a7b9333a74",
+    "url": "/instagram-clone/static/js/main.cb229092.chunk.js"
+  },
+  {
+    "revision": "7e1399c33abb191599e5",
+    "url": "/instagram-clone/static/js/runtime-main.8a002ec6.js"
+  }
+]);
