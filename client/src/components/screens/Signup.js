b830ba7771cc70d4d283265afac9b009bc59acec
Himani <oyo@MacBook-Pro-64.local>
2020-09-15 17:06:56 +0530

    check

--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {Link} from 'react-router-dom'
 
 const Signup = () => {
+    const [name,setName] = useState("")
+    const [password,setPassword] = useState("")
+    const [email,setEmail] = useState("")
+
+    const PostData=()=>{
+        //network req to server
+        fetch("http://localhost:5000/signup",{
+            method:"post",
+            headers:{
+                "Content-Type":"application/json"
+            }
+        }
+    )}
+
     return (
         <div className="mycard">
             <div className="auth-card">
@@ -10,16 +24,22 @@ const Signup = () => {
                     type="text"
                     placeholder="Username"
                     class="inputstyle"
+                    value={name}
+                    onChange={(e)=>setName(e.target.value)}
                 />
                 <input
                     type="text"
                     placeholder="Email"
                     class="inputstyle"
+                    value={email}
+                    onChange={(e)=>setEmail(e.target.value)}
                 />
                 <input
                     type="text"
                     placeholder="Password"
                     class="inputstyle"
+                    value={password}
+                    onChange={(e)=>setPassword(e.target.value)}
                 />
                 <button className="btn waves-effect waves-light #64b5f6 blue">
                     Signup
